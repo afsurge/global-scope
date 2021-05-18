3ace0e1a8d87dc3a0d0ce5ba7dea796be8727e9d
Abrar Faisal <abrarfaisal20@gmail.com>
2021-05-18 23:39:48 +0200

    Files removed

--- andrea/greetee.js
+++
@@ -1,4 +0,0 @@
-export default function Greetee(props) {
-    console.log("props in greetee:", props);
-    return <span>{props.name || "FENNEL FRIENDS"}</span>;
-}
--- andrea/start-andrea.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom";
-import HelloWorld from "./helloworld";
-
-ReactDOM.render(<HelloWorld />, document.querySelector("main"));
