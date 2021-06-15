6f9f820b82f22d85050b73cb390f650cf627d416
Abrar Faisal <abrarfaisal20@gmail.com>
2021-06-15 17:19:55 +0200

    RTL testing with ProfilePic

---
+++ client/src/profilepic.test.js
@@ -0,0 +1,41 @@
+import ProfilePic from "./profilepic";
+import { render, fireEvent } from "@testing-library/react";
+import { expect } from "@jest/globals";
+
+test("img has correct src when passed a url", () => {
+    const { container } = render(<ProfilePic imgUrl="/snowyhusky.jpg" />);
+
+    expect(container.querySelector("img").src.endsWith("/snowyhusky.jpg")).toBe(
+        true
+    );
+});
+
+test("img has correct src when no url passed", () => {
+    const { container } = render(<ProfilePic />);
+
+    expect(
+        container.querySelector("img").src.endsWith("no-profile-pic.png")
+    ).toBe(true);
+});
+
+test("img has correct alt attribute", () => {
+    const { container } = render(<ProfilePic first="Abrar" last="Faisal" />);
+
+    expect(container.querySelector("img").alt).toBe("Faisal");
+});
+
+test("onClick prop gets attached as event listener to img", () => {
+    const toggleUploader = jest.fn();
+
+    const { container } = render(
+        <ProfilePic toggleUploader={toggleUploader} />
+    );
+
+    console.log(toggleUploader.mock.calls);
+
+    fireEvent.click(container.querySelector("img"));
+
+    fireEvent.click(container.querySelector("img"));
+
+    expect(toggleUploader.mock.calls.length).toBe(2);
+});
