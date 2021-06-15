a2fc947c16f36aa35f2d2f92ddf10812db6d4f93
Abrar Faisal <abrarfaisal20@gmail.com>
2021-06-15 17:56:23 +0200

    Update RTL with BioEditor incomplete

--- client/src/bioeditor.test.js
+++ client/src/bioeditor.test.js
@@ -1,5 +1,5 @@
 import BioEditor from "./bioeditor";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import axios from "./axios";
 
 test("ADD button is rendered when no Bio is passed", () => {
@@ -9,7 +9,21 @@ test("ADD button is rendered when no Bio is passed", () => {
 });
 
 test("EDIT button is rendered when Bio is passed", () => {
-    const { container } = render(<BioEditor bio="Batman Fandom" />);
+    const { container } = render(<BioEditor bio="Dark Knight Rises" />);
 
     expect(container.querySelector("button").innerHTML).toBe("EDIT");
+});
+
+test("textarea and SAVE button is rendered when ADD or EDIT button is clicked", () => {
+    // without bio passed
+    // const { container } = render(<BioEditor />);
+
+    // with bio passed
+    const { container } = render(<BioEditor bio="Dark Knight Rises" />);
+
+    fireEvent.click(container.querySelector("button"));
+
+    expect(container.querySelector("textarea")).toBeTruthy();
+    expect(container.querySelectorAll("button").length).toBe(2);
+    expect(container.querySelectorAll("button")[1].innerHTML).toBe("SAVE");
 });
